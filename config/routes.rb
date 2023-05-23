76e360506cbdc37806559023cf35d70eeac9d7dd
Ubuntu <ubuntu@ip-172-31-30-3.ap-northeast-1.compute.internal>
2023-05-23 14:00:38 +0000

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :todos
-  resources :goals
+  resources :goals do
+    resources :todos do
+      # /goals/:goal_id/todos/:id/sort というURLに対して
+      # todos#sort というアクションを割り当てる
+      get "sort"
+    end
+  end
+      
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "home#index"
